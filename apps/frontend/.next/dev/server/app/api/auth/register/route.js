4d6133d7e6d5a6648408fe28d802bb138cbd1f89
Shivam Kumar <kumarys8922@gmail.com>
2026-07-14 13:36:36 +0530

    change3

---
+++ apps/frontend/.next/dev/server/app/api/auth/register/route.js
@@ -0,0 +1,7 @@
+var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/auth/register/route.js")
+R.c("server/chunks/apps_frontend_app_api_auth_register_route_ts_1ed6ktm._.js")
+R.c("server/chunks/[root-of-the-server]__17gh-o-._.js")
+R.c("server/chunks/[root-of-the-server]__1l33kz9._.js")
+R.c("server/chunks/apps_frontend__next-internal_server_app_api_auth_register_route_actions_11ua05x.js")
+R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/apps/frontend/app/api/auth/register/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/apps/frontend/app/api/auth/register/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
